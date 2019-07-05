12e237413782c56f35d50b95cf6c245c44aafe59
Shivam Agrawal <shivamagrawal@Shivams-MacBook-Air.local>
2019-07-05 11:48:06 +0530

    add post script

---
+++ postcommit.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+branch=`git rev-parse --abbrev-ref HEAD`
+echo ${branch}
+if [[ "${branch}" == "master" ]]
+then
+npm run release 
+#git push --follow-tags origin ${branch}
+fi
+
+if [[ "${branch}" == "testing" ]] 
+then
+npm run release -- --prerelease beta
+exit 0;
+#git push --follow-tags origin ${branch}
+fi
